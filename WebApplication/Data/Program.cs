db491f2fb16cddfbfe193d2c100f012ef7a357db
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2021-12-21 00:30:06 +0100

    Created blazor sample project and merged with main branch

---
+++ WebApplication/Data/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System;
+using static Spotify.SpotifyModule;
+using static Spotify.YouTubeModule;
+
+namespace Spotify
+{
+  public static class Program
+  {
+    
+    private static void Exiting() => Console.CursorVisible = true;
+    public static async Task<int> Main()
+    {
+      AppDomain.CurrentDomain.ProcessExit += (sender, e) => Exiting();
+      
+      if (File.Exists(CredentialsPath))
+      {
+        var list = await Start();
+        var youTubeModule = new YouTubeModule();
+        await youTubeModule.GeneratePlaylist(await youTubeModule.GoogleAuth(),list);
+      }
+      else
+      {
+        await StartAuthentication();
+      }
+
+      Console.ReadKey();
+      return 0;
+    }
+
+  }
+}
